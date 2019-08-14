0851babf2454030605a5ba5393fd2a0335e26a6a
Dom <djgseymour@gmail.com>
2019-08-14 13:47:54 +0100

    Adding report functionality via cucumber options in TestRunner class

--- src/test/java/dataProviders/ConfigFileReader.java
+++ src/test/java/dataProviders/ConfigFileReader.java
@@ -84,4 +84,11 @@ public class ConfigFileReader {
         else throw new RuntimeException("Test data resources path not specified in Configuration properties file.");
     }
 
+    public String getReportConfigPath(){
+        String reportConfigPath = properties.getProperty("reportConfigPath");
+        if(reportConfigPath!= null)
+            return reportConfigPath;
+        else throw new RuntimeException("Report Config Path not specified in the Configuration.properties file for the Key:reportConfigPath");
+    }
+
 }
--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -3,13 +3,19 @@ package testRunners;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import managers.FileReaderManager;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@new_test"
+        tags = "@new_test",
+        monochrome = true,
+        plugin = { "pretty", "html:target/cucumber-reports" }
 )
 
 public class TestRunner {
